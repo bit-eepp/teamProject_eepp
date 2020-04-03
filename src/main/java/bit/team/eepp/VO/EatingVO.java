3b7916596498d819cb7c6cc3501c0497e39d3cca
rtnbrnch <rtnbrnch@gmail.com>
2020-04-03 17:00:50 +0900

    찬경_eating table 수정에 따른 eatingVo, reviewVo, reviewController 변경(04/03)

--- src/main/java/bit/team/eepp/VO/EatingVO.java
+++ src/main/java/bit/team/eepp/VO/EatingVO.java
@@ -1,33 +1,32 @@
 package bit.team.eepp.VO;
 
+import java.sql.Timestamp;
+
 public class EatingVO {
 	
-	private int eId;
-	private int user_id;
+	private int eId; 
+	private int User_id;
 	private String eTitle;
 	private String eCategory;
-	private String eDate;
+	private Timestamp eDate; 
 	private String eContent;
 	private String eTel;
 	private String eAddress_new;
-	private String eAddress_old;
-	private String eGrade;
-	private String eMap_1;
-	private String eMap_2;
-	private String eSearch_1;
-	private String eSearch_2;
-	private String eSearch_3;
+	private String eAddress_old; 
+	private String eThema;
+	private String eKeyword_food;
 	private int rvCount;
+	private float rvAVG;
 	
 	public EatingVO(){
 		
 	}
 
-	public EatingVO(int eId, int user_id, String eTitle, String eCategory, String eDate, String eContent, String eTel,
-			String eAddress_new, String eAddress_old, String eGrade, String eMap_1, String eMap_2, String eSearch_1,
-			String eSearch_2, String eSearch_3, int rvCount) {
+	public EatingVO(int eId, int user_id, String eTitle, String eCategory, Timestamp eDate, String eContent,
+			String eTel, String eAddress_new, String eAddress_old, String eThema, String eKeyword_food, int rvCount,
+			float rvAVG) {
 		this.eId = eId;
-		this.user_id = user_id;
+		User_id = user_id;
 		this.eTitle = eTitle;
 		this.eCategory = eCategory;
 		this.eDate = eDate;
@@ -35,21 +34,10 @@ public class EatingVO {
 		this.eTel = eTel;
 		this.eAddress_new = eAddress_new;
 		this.eAddress_old = eAddress_old;
-		this.eGrade = eGrade;
-		this.eMap_1 = eMap_1;
-		this.eMap_2 = eMap_2;
-		this.eSearch_1 = eSearch_1;
-		this.eSearch_2 = eSearch_2;
-		this.eSearch_3 = eSearch_3;
+		this.eThema = eThema;
+		this.eKeyword_food = eKeyword_food;
 		this.rvCount = rvCount;
-	}
-
-	public int getRvCount() {
-		return rvCount;
-	}
-
-	public void setRvCount(int rvCount) {
-		this.rvCount = rvCount;
+		this.rvAVG = rvAVG;
 	}
 
 	public int geteId() {
@@ -61,11 +49,11 @@ public class EatingVO {
 	}
 
 	public int getUser_id() {
-		return user_id;
+		return User_id;
 	}
 
 	public void setUser_id(int user_id) {
-		this.user_id = user_id;
+		User_id = user_id;
 	}
 
 	public String geteTitle() {
@@ -84,11 +72,11 @@ public class EatingVO {
 		this.eCategory = eCategory;
 	}
 
-	public String geteDate() {
+	public Timestamp geteDate() {
 		return eDate;
 	}
 
-	public void seteDate(String eDate) {
+	public void seteDate(Timestamp eDate) {
 		this.eDate = eDate;
 	}
 
@@ -124,55 +112,36 @@ public class EatingVO {
 		this.eAddress_old = eAddress_old;
 	}
 
-	public String geteGrade() {
-		return eGrade;
+	public String geteThema() {
+		return eThema;
 	}
 
-	public void seteGrade(String eGrade) {
-		this.eGrade = eGrade;
+	public void seteThema(String eThema) {
+		this.eThema = eThema;
 	}
 
-	public String geteMap_1() {
-		return eMap_1;
+	public String geteKeyword_food() {
+		return eKeyword_food;
 	}
 
-	public void seteMap_1(String eMap_1) {
-		this.eMap_1 = eMap_1;
+	public void seteKeyword_food(String eKeyword_food) {
+		this.eKeyword_food = eKeyword_food;
 	}
 
-	public String geteMap_2() {
-		return eMap_2;
+	public int getRvCount() {
+		return rvCount;
 	}
 
-	public void seteMap_2(String eMap_2) {
-		this.eMap_2 = eMap_2;
+	public void setRvCount(int rvCount) {
+		this.rvCount = rvCount;
 	}
 
-	public String geteSearch_1() {
-		return eSearch_1;
+	public float getRvAVG() {
+		return rvAVG;
 	}
 
-	public void seteSearch_1(String eSearch_1) {
-		this.eSearch_1 = eSearch_1;
+	public void setRvAVG(float rvAVG) {
+		this.rvAVG = rvAVG;
 	}
-
-	public String geteSearch_2() {
-		return eSearch_2;
-	}
-
-	public void seteSearch_2(String eSearch_2) {
-		this.eSearch_2 = eSearch_2;
-	}
-
-	public String geteSearch_3() {
-		return eSearch_3;
-	}
-
-	public void seteSearch_3(String eSearch_3) {
-		this.eSearch_3 = eSearch_3;
-	}
-	
-	
-	
 	
 }
